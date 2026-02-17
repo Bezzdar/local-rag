bba17b75f4955581119812b9016336bda1efbff2
impumpiu-cpu <impump.iu@gmail.com>
2026-02-17 09:03:25 +0700

    refactor: удалить legacy директории и синхронизировать README

--- app/config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-DOCS_PATH = os.path.join(BASE_DIR, "data", "docs")
-INDEX_ROOT = os.path.join(BASE_DIR, "data", "index")
